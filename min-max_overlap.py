a0e0b2183adc5aa8d479f9126cb3bde80c0b04fd
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2021-02-20 12:52:20 -0500

    Update min-max_overlap.py and counts
    Remaining bug is stray '--' in the summary.txt file generated by counts.

--- min-max_overlap.py
+++ min-max_overlap.py
@@ -9,23 +9,10 @@ from pgconnection import PgConnection
 from format_rules import _grade
 
 conn = PgConnection()
-first_cursor = conn.cursor()  # Finds courses with ambiguous courses
-format_cursor = conn.cursor()  # Used to format a rule
+first_cursor = conn.cursor()  # Used to find potentially ambiguous rules
+second_cursor = conn.cursor()  # Used to lookup source_courses and, later, destination courses
 
-SrcCourse = namedtuple('SrcCourse', 'course_id offer_nbr discipline catalog_number '
-                       'min_gpa max_gpa designation course_status')
-DstCourse = namedtuple('DstCourse', 'course_id offer_nbr is_msg is_bkcr course_status')
-RuleInfo = namedtuple('RuleInfo', 'src_courses dst_courses text')
-
-
-# init_rule_info()
-# -------------------------------------------------------------------------------------------------
-def init_rule_info():
-  """ Initialize structure for recording info about ambiguous rules as a tuple of two sets.
-      Set[0] is SrcCourses; set[1] is DstCourses
-  """
-  return RuleInfo._make([set(), set(), []])
-
+Rule = namedtuple('Rule', 'id key')
 
 # key_order()
 # -------------------------------------------------------------------------------------------------
@@ -53,80 +40,50 @@ def format_range(course, ambiguous_low, ambiguous_high):
   return f'{range}{preposition}{course.discipline} {course.catalog_number}'
 
 
-# format_rule()
+# format_rules()
 # -------------------------------------------------------------------------------------------------
-def format_rule(rule_id: int) -> str:
-  """ Return string description of a transfer rule.
+def format_rules(pair: tuple, sending_courses: tuple) -> list:
+  """ Generate list of additional info for two rules
+      The info for each rule consists of:
+        The textual representations of the two rules
+        Lists of receiving courses with their designations, attributes, and course_statuses for the
+        two rules.
   """
-  format_cursor.execute(f'select rule_key(id) from transfer_rules where id = {rule_id}')
-  rule_key = format_cursor.fetchone().rule_key
+  assert len(pair) == 2
+  rules_info = [dict(), dict()]
+  rules_info[0]['text'] = ' and '.join([f'{_grade(c.min_gpa, c.max_gpa)} {c.discipline} '
+                                        f'{c.catalog_number} [{c.course_id:06}.'
+                                        f'{c.offer_nbr} {c.course_status}]'
+                                        for c in sending_courses[0]]) + ' => '
+  rules_info[1]['text'] = ' and '.join([f'{_grade(c.min_gpa, c.max_gpa)} {c.discipline} '
+                                        f'{c.catalog_number} [{c.course_id:06}.'
+                                        f'{c.offer_nbr} {c.course_status}]'
+                                        for c in sending_courses[1]]) + ' => '
 
-  # Gather the set of sending courses for the rule
-  format_cursor.execute(f"""
-select  s.course_id, s.offer_nbr,
-        s.discipline,
-        s.catalog_number,
-        min_gpa,
-        max_gpa,
-        c.designation,
-        c.course_status
-  from source_courses s, cuny_courses c
-  where s.rule_id = {rule_id}
-    and c.course_id = s.course_id
-    and c.offer_nbr = s.offer_nbr
-  order by s.discipline, numeric_part(s.catalog_number)
-                        """)
-  courses = format_cursor.fetchall()
-  for course in courses:
-    # print(SrcCourse._make([course.course_id,
-    rule_info[rule_key].src_courses.add(SrcCourse._make([course.course_id,
-                                                         course.offer_nbr,
-                                                         course.discipline,
-                                                         course.catalog_number,
-                                                         course.min_gpa,
-                                                         course.max_gpa,
-                                                         course.designation,
-                                                         course.course_status]))
-
-  returnVal = ' and '.join([f'{_grade(c.min_gpa, c.max_gpa)} {c.discipline} {c.catalog_number} '
-                            f'[{c.course_id:06}.{course.offer_nbr} {c.course_status}]'
-                            for c in courses])
-  returnVal += ' => '
-
-  # Gather the set of receiving courses for the rule
-  format_cursor.execute(f"""
+  # Gather the set of receiving courses for the two rules
+  for index in range(2):
+    second_cursor.execute(f"""
 select  d.course_id,
         d.offer_nbr,
         d.discipline,
         d.catalog_number,
-        c.designation,
-        c.attributes,
+        c.designation in ('MLA', 'MNL') as is_mesg,
+        c.attributes ~ 'BKCR' as is_bkcr,
         c.course_status
   from destination_courses d, cuny_courses c
-  where rule_id = {rule_id}
+  where rule_id = {pair[index].id}
     and d.course_id = c.course_id
     and d.offer_nbr = c.offer_nbr
                         """)
-  dest_list = []
-  courses = format_cursor.fetchall()
-  # Add each receiving course to rule_info[rule_key][1]
-  for course in courses:
-    # if rule_key == 'BAR01:QNS01:BIO:1':
-    #   print(course, file=sys.stderr)
-    rule_info[rule_key].dst_courses.add(DstCourse._make([course.course_id,
-                                                         course.offer_nbr,
-                                                         (course.designation == 'MLA'
-                                                          or course.designation == 'MNL'),
-                                                         'BKCR' in course.attributes,
-                                                        course.course_status]))
-    dest_list.append(f'{course.discipline} {course.catalog_number} '
-                     f'[{course.course_id:06}.{course.offer_nbr} {course.course_status}]')
-  returnVal += ' and '.join(dest_list)
-  rule_info[rule_key].text.append(returnVal)
-  # if rule_key == 'BAR01:QNS01:BIO:1':
-  #   print(rule_info[rule_key], file=sys.stderr)
-  #   exit()
-  return returnVal
+    dest_courses = second_cursor.fetchall()
+    rules_info[index]['dest_courses'] = dest_courses
+    rules_info[index]['text'] += ' and '.join([f'{course.discipline} '
+                                               f'{course.catalog_number} '
+                                               f'[{course.course_id:06}.'
+                                               f'{course.offer_nbr} '
+                                               f'{course.course_status}]'
+                                               for course in dest_courses])
+  return rules_info
 
 
 if __name__ == '__main__':
@@ -136,58 +93,69 @@ if __name__ == '__main__':
   parser.add_argument('-p', '--progress', action='store_true', default=False)
   args = parser.parse_args()
 
-  rule_info = defaultdict(init_rule_info)
-  ambiguous_pairs = []
-  course_rules = defaultdict(set)
-
   if args.progress:
-    print('Find problem rules')
+    print('Lookup problem rules', file=sys.stderr)
   first_cursor.execute("""
--- Get source courses that appear in pairs of rules for the same destination institution.
-select s1.course_id, s1.offer_nbr, s1.discipline, s1.catalog_number, c.course_status,
-       s1.min_gpa as min_1, s1.max_gpa as max_1,
-       r1.id as r1_id, rule_key(r1.id) as key_1, r1.priority as priority_1,
-       s2.min_gpa as min_2, s2.max_gpa as max_2,
-       r2.id as r2_id, rule_key(r2.id) as key_2, r2.priority as priority_2
-  from source_courses s1, source_courses s2, transfer_rules r1, transfer_rules r2,cuny_courses c
-  where s1.course_id = s2.course_id
-    and s1.offer_nbr = s2.offer_nbr
-    and s1.course_id = c.course_id
-    and s2.offer_nbr = c.offer_nbr
-    and s1.rule_id = r1.id
-    and s2.rule_id = r2.id
-    and r1.id < r2.id
-    and r1.destination_institution = r2.destination_institution
-  order by course_id
-                 """)
+-- Look up potential ambiguities: cases where the only way to tell which rule applies would be a
+-- difference in grade requirements.
 
-  # Filter for cases where there is no way to tell which rule should apply.
-  with open('./min-max_overlap.csv', 'w') as mmo_file:
-    print('course_id,course,min_1,max_1,rule 1,min_2,max_2,rule 2', file=mmo_file)
-    m = 0
-    n = first_cursor.rowcount
-    for row in first_cursor.fetchall():
-      if args.progress:
-        if m == 0:
-          print('Build rule pairs')
-        m += 1
-        print(f' {m:06,}/{n:06,}\r', end='')
-      course = (row.course_id, row.offer_nbr)
-      if row.priority_1 == row.priority_2 and (row.max_1 > row.min_2 or row.max_2 > row.min_1):
-        # Update info for this pair of rules
-        ambiguous_pairs.append((row.key_1, row.key_2))
-        # Add row for this course to CSV report
-        if row.r2_id in course_rules[row.course_id] and row.r2_id in course_rules[row.course_id]:
-          # Skip rules already seen for this course
-          continue
-        course_rules[course].add(row.r1_id)
-        course_rules[course].add(row.r2_id)
-        text_1 = format_rule(row.r1_id)
-        text_2 = format_rule(row.r2_id)
-        print(f'[{row.course_id:06}.{row.offer_nbr} {row.course_status}], '
-              f'{row.discipline} {row.catalog_number},'
-              f'{row.min_1}, {row.max_1}, {row.key_1}: {text_1}, '
-              f'{row.min_2}, {row.max_2}, {row.key_2}: {text_2}', file=mmo_file)
+select r1.id as id_1, r1.rule_key as key_1,
+       r2.id as id_2, r2.rule_key as key_2,
+       r1.sending_courses,
+       r1.receiving_courses as receiving_1,
+       r2.receiving_courses as receiving_2
+from transfer_rules r1, transfer_rules r2
+where r1.id < r2.id
+and r1.destination_institution = r2.destination_institution
+and r1.sending_courses = r2.sending_courses
+and r1.priority = r2.priority
+order by r1.rule_key, r2.rule_key
+
+""")
+
+  # Lookup courses involved and generate string represenation of the rules
+
+  m = 0
+  n = first_cursor.rowcount
+  ambiguous_pairs = set()   # Rule_key pairs
+  source_info = defaultdict(tuple)   # Pair of source course lookups indexed by rule_key pair
+  for row in first_cursor.fetchall():
+    if args.progress:
+      if m == 0:
+        print('Format rule pairs', file=sys.stderr)
+      m += 1
+      print(f' {m:06,}/{n:06,}\r', end='', file=sys.stderr)
+      # Lookup the sending course info for the two rules.
+      second_cursor.execute(f"""
+select s.*, c.course_status, c.designation, c.attributes
+  from source_courses s, cuny_courses c
+ where rule_id = {row.id_1}
+   and c.course_id = s.course_id
+   and c.offer_nbr = s.offer_nbr
+  order by course_id, offer_nbr
+""")
+      r1_sending_courses = second_cursor.fetchall()
+
+      second_cursor.execute(f"""
+select s.*, c.course_status, c.designation, c.attributes
+  from source_courses s, cuny_courses c
+ where rule_id = {row.id_2}
+   and c.course_id = s.course_id
+   and c.offer_nbr = s.offer_nbr
+  order by course_id, offer_nbr
+""")
+      r2_sending_courses = second_cursor.fetchall()
+
+      # The rules are ambiguous if one or more courses have overlapping grade requirements.
+      assert len(r1_sending_courses) == len(r2_sending_courses)
+      for index in range(len(r1_sending_courses)):
+        assert (r1_sending_courses[index].course_id == r2_sending_courses[index].course_id
+                and r1_sending_courses[index].offer_nbr == r2_sending_courses[index].offer_nbr)
+        if r1_sending_courses[index].max_gpa > r2_sending_courses[index].min_gpa or \
+           r2_sending_courses[index].max_gpa > r1_sending_courses[index].min_gpa:
+          pair = (Rule._make([row.id_1, row.key_1]), Rule._make([row.id_2, row.key_2]))
+          ambiguous_pairs.add(pair)
+          source_info[pair] = (r1_sending_courses, r2_sending_courses)
 
   # Generate Report
   """ For each pair of rules that includes at least one course ambiguity, report:
@@ -202,60 +170,52 @@ select s1.course_id, s1.offer_nbr, s1.discipline, s1.catalog_number, c.course_st
   n = len(ambiguous_pairs)
   today_str = date.today().strftime('%Y-%m-%d')
   with open(f'./{today_str}_rules_report.txt', 'w') as report_file:
-    for pair in sorted(ambiguous_pairs, key=lambda x: key_order(x[0])):
+    for pair in sorted(ambiguous_pairs, key=lambda x: key_order(x[0].key)):
       if args.progress:
         if m == 0:
           print('Analyze rule pairs')
         m += 1
-        print(f' {m:06,}/{n:06,}\r', end='')
+        print(f' {m:06,}/{n:06,}\r', end='', file=sys.stderr)
 
-      # Ignore pairs where there are different sets of sending courses
-      if rule_info[pair[0]].src_courses != rule_info[pair[1]].src_courses:
-        print(f'{pair}: {rule_info[pair[0]].src_courses} != {rule_info[pair[1]].src_courses}\n')
-        continue
+      # Fill out the information for the rule
+      rules_info = format_rules(pair, source_info[pair])
 
       # Tell what rule-pair is being reported on
-      print(f'\n{pair[0]:24} {rule_info[pair[0]].text[0]}\n'
-            f'{pair[1]:24} {rule_info[pair[1]].text[0]}', file=report_file)
+      print(f'\n{pair[0].key:24} {rules_info[0]["text"]}\n'
+            f'{pair[1].key:24} {rules_info[1]["text"]}', file=report_file)
 
       # What sending courses are problematic, and what is the range of overlap?
       ambiguities = []
-      for course_1 in rule_info[pair[0]].src_courses:
-        for course_2 in rule_info[pair[1]].src_courses:
-          if (course_1.course_id, course_1.offer_nbr) == (course_2.course_id, course_2.offer_nbr):
-            if course_1.max_gpa > course_2.min_gpa or course_2.max_gpa > course_1.min_gpa:
-              ambiguous_low = max(course_1.min_gpa, course_2.min_gpa)
-              ambiguous_high = min(course_1.max_gpa, course_2.max_gpa)
-              ambiguities.append(format_range(course_1, ambiguous_low, ambiguous_high))
+      for course_1 in source_info[pair][0]:
+        for course_2 in source_info[pair][1]:
+          if course_1.max_gpa > course_2.min_gpa or course_2.max_gpa > course_1.min_gpa:
+            ambiguous_low = max(course_1.min_gpa, course_2.min_gpa)
+            ambiguous_high = min(course_1.max_gpa, course_2.max_gpa)
+            ambiguities.append(format_range(course_1, ambiguous_low, ambiguous_high))
       if len(ambiguities) == 1:
         print(f'  Grade ambiguity: {ambiguities[0]}', file=report_file)
       else:
         ambiguity_str = '; '.join(ambiguities)
         print(f'  Grade ambiguities: {ambiguity_str}', file=report_file)
 
-      # Are the source and destination course sets the same for the two rules?
-      source_1 = set([(rule.course_id, rule.offer_nbr) for rule in rule_info[pair[0]].src_courses])
-      source_2 = set([(rule.course_id, rule.offer_nbr) for rule in rule_info[pair[1]].src_courses])
-      dest_1 = set([(rule.course_id, rule.offer_nbr) for rule in rule_info[pair[0]].dst_courses])
-      dest_2 = set([(rule.course_id, rule.offer_nbr) for rule in rule_info[pair[1]].dst_courses])
-      if source_1 == source_2 and dest_1 == dest_2:
-        same_comp = 'same' if pair[0].split(':')[2] == pair[1].split(':')[2] else 'different'
-        print(f'  Same sending and receiving courses with {same_comp} component subject areas',
-              file=report_file)
-      elif source_1 == source_2:
-        print(f'  Same sending courses; different receiving courses', file=report_file)
-      elif dest_1 == dest_2:
-        print(f'  Different sending courses; same receiving courses', file=report_file)
-      else:
-        print(f'  Different sending and receiving courses', file=report_file)
+      # How do the sets of receiving courses compare? (We know the sending course sets and the rule
+      # priorities are the same.)
+      r1_component = pair[0].key.split(':')[2]
+      r2_component = pair[1].key.split(':')[2]
+      same_comp = 'Same' if r1_component == r2_component else 'Different'
+      dest_1 = set([(rule.course_id, rule.offer_nbr) for rule in rules_info[0]['dest_courses']])
+      dest_2 = set([(rule.course_id, rule.offer_nbr) for rule in rules_info[1]['dest_courses']])
+      same_dest = 'same' if dest_1 == dest_2 else 'different'
+      print(f'  {same_comp} component subject areas and {same_dest} receiving courses',
+            file=report_file)
 
       # Are receiving courses BKCR and/or MSG?
       all_blanket_1 = True
       all_message_1 = True
       any_blanket_1 = False
       any_message_1 = False
-      for course in rule_info[pair[0]].dst_courses:
-        if course.is_msg:
+      for course in rules_info[0]['dest_courses']:
+        if course.is_mesg:
           any_message_1 = True
         else:
           all_message_1 = False
@@ -268,8 +228,8 @@ select s1.course_id, s1.offer_nbr, s1.discipline, s1.catalog_number, c.course_st
       all_message_2 = True
       any_blanket_2 = False
       any_message_2 = False
-      for course in rule_info[pair[1]].dst_courses:
-        if course.is_msg:
+      for course in rules_info[1]['dest_courses']:
+        if course.is_mesg:
           any_message_2 = True
         else:
           all_message_2 = False
@@ -306,7 +266,6 @@ select s1.course_id, s1.offer_nbr, s1.discipline, s1.catalog_number, c.course_st
         print(f'  First rule is not BKCR; Second rule is all BKCR', file=report_file)
       elif any_blanket_2:
         print(f'  First rule is not BKCR; Second rule is part BKCR', file=report_file)
-
       else:
         print(f'  Neither rule is BKCR', file=report_file)
 
@@ -328,7 +287,6 @@ select s1.course_id, s1.offer_nbr, s1.discipline, s1.catalog_number, c.course_st
         print(f'  First rule is not MESG; Second rule is all MESG', file=report_file)
       elif any_message_2:
         print(f'  First rule is not MESG; Second rule is part MESG', file=report_file)
-
       else:
         print(f'  Neither rule is MESG', file=report_file)
 
